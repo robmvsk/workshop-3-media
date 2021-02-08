3d91c3c85eaef3db3f9abd7c1544e75d11d0f1b1
RobM <robskdeveloper@gmail.com>
2021-02-08 11:27:13 -0600

    proyecto workshop 3 - media player

---
+++ snowpack.config.js
@@ -0,0 +1,10 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  buildOptions: {
+    baseUrl: 'https://robmvsk.github.io/workshop-3-media',
+  },
+}
